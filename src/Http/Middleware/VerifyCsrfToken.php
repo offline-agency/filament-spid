ccd291faec9676eb369cb3242bb92252145704c9
Giacomo92 <info@giacomofabbian.it>
2025-10-16 17:39:36 +0200

    fix: php stan errors

--- src/Http/Middleware/VerifyCsrfToken.php
+++ src/Http/Middleware/VerifyCsrfToken.php
@@ -3,13 +3,20 @@
 namespace OfflineAgency\FilamentSpid\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\Foundation\Application;
 
 class VerifyCsrfToken extends Middleware
 {
+    public function __construct(Application $app, Encrypter $encrypter)
+    {
+        parent::__construct($app, $encrypter);
+    }
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $except = [
         '*/spid/*',
